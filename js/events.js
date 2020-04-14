ce49d0f8786ce35ce55c05279a1dfcc293e81ded
Kevin Boston <kbos22@gmail.com>
2020-04-14 16:25:22 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,17 @@
 //define functions here
-
+function getIt (){
+  return $(document).on('click', function(){
+    alert('Hey!');
+  } );
+}
+function frameIt(){
+  return $(document).on('load', function(){
+    
+  });
+}
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
